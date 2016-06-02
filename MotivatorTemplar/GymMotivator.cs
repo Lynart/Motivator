abdd3068f14c046a5ee794c5bd2ca7e748adb57b
Vince Lee <vlee@BES.blackstoneenergy.com>
2016-06-02 16:51:30 -0400

    Forgot a file

---
+++ MotivatorTemplar/GymMotivator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+
+namespace MotivatorTemplar
+{
+    class GymMotivator
+    {
+        DateTime clavicleBreakDate;
+
+        //Config needed to grab values from app.config
+        Int32 deadlift;
+        Int32 squat;
+        Int32 bench;
+        Int32 shoulder;
+        Int32 clean;
+
+        Configuration config; //needed for saving?
+
+        public GymMotivator()
+        {
+            clavicleBreakDate = new DateTime(2014, 8, 30);
+            deadlift = Convert.ToInt32(ConfigurationManager.AppSettings["Deadlift"]);
+            squat = Convert.ToInt32(ConfigurationManager.AppSettings["Squat"]);
+            bench = Convert.ToInt32(ConfigurationManager.AppSettings["Bench"]);
+            shoulder = Convert.ToInt32(ConfigurationManager.AppSettings["Shoulder"]);
+            clean = Convert.ToInt32(ConfigurationManager.AppSettings["Clean"]);
+
+            string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string configFile = System.IO.Path.Combine(appPath, "App.config");
+            ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+            configFileMap.ExeConfigFilename = configFile;
+            config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+        }
+
+        public void setDeadlift(int lifted)
+        {
+            config.AppSettings.Settings["Deadlift"].Value = lifted.ToString();
+            config.Save();
+        }
+
+        public void setSquat(int lifted)
+        {
+
+        }
+
+        public void setBench(int lifted)
+        {
+
+        }
+
+        public void setShoulder(int lifted)
+        {
+
+        }
+
+        public void setClean(int lifted)
+        {
+
+        }
+    }
+}
